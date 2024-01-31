fb2a5fa9378509fc3d50714acf0c5bfd3af8eeb0
aMand7 <amanddseventaurus@gmail.com>
2024-01-31 22:24:44 +0700

    feat: menambahkan kata yang diterjemahkan pada useLocale

--- src/features/auth/login/pages/Login.tsx
+++ src/features/auth/login/pages/Login.tsx
@@ -1,7 +1,14 @@
 import NavAuth from '../../../../components/nav/NavAuth.tsx'
 import FormLogin from '../components/FormLogin.tsx'
+import useAuth from '../../../../hooks/useAuth'
+import { Navigate } from 'react-router-dom'
 
 const Login = () => {
+  const { isAuthenticated } = useAuth()
+
+  if (isAuthenticated) {
+    return <Navigate to='/' replace />
+  }
 
   return (
     <>
