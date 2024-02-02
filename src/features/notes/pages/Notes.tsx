659b71e127ffa58bdacecca0f34a66e876025a60
aMand7 <amanddseventaurus@gmail.com>
2024-02-02 15:44:57 +0700

    refactor: meringkas penggunaan useMutation archive dan unarchive pada halaman detail notes

--- src/features/notes/pages/Notes.tsx
+++ src/features/notes/pages/Notes.tsx
@@ -41,7 +41,7 @@ const Notes = () => {
             ))
           : isLoading && <NotesSkeleton loop={9} />}
         {activeNotes?.data?.length < 1 && !isLoading && (
-          <InfoNotes info={isLocale === 'id' ? 'Tidak ada catatan.' : 'No notes.'} />
+          <InfoNotes info={isLocale === 'id' ? 'Tidak ada catatan.' : 'Empty notes.'} />
         )}
       </div>
     </>
